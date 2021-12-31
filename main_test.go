26b198821c3eafba122c18530d1119944a5d0ee7
karna <karnas37@gmail.com>
2021-12-31 11:16:15 +0530

    Test getTodos handler

--- main_test.go
+++ main_test.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func makeRequest(method string, route string, body io.Reader) (httptest.ResponseRecorder, error) {
-	req, err := http.NewRequest(method, route, body)
+type request struct {
+	method string
+	route  string
+	body   io.Reader
+}
+
+func makeRequest(requst request, handleFunc func(http.ResponseWriter, *http.Request)) (httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(requst.method, requst.route, requst.body)
 
 	rr := httptest.NewRecorder()
 
 	if err != nil {
 		return *rr, err
 	}
-	handler := http.HandlerFunc(homeLink)
+	handler := http.HandlerFunc(handleFunc)
 	handler.ServeHTTP(rr, req)
 
 	return *rr, nil
@@ -58,11 +65,65 @@ func Test_homeLink(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			reponse, err := makeRequest(tt.args.method, "/", nil)
+			reponse, err := makeRequest(request{tt.args.method, "/", nil}, homeLink)
 
 			if err != nil {
 				t.Fatal(err)
 			}
+
+			if status := reponse.Code; status != tt.args.expCode {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, tt.args.expCode)
+			}
+			if reponse.Body.String() != tt.args.expBody {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					reponse.Body.String(), tt.args.expBody)
+			}
+		})
+	}
+}
+
+func Test_getTodos(t *testing.T) {
+	todo, err := json.Marshal(todos)
+
+	if err != nil {
+		t.Errorf("Something went wrong %v", err)
+	}
+
+	todoString := string(todo)
+
+	type args struct {
+		method  string
+		expCode int
+		expBody string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				method:  "GET",
+				expCode: http.StatusOK,
+				expBody: todoString,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reponse, err := makeRequest(request{tt.args.method, "/todos", nil}, getTodos)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// var body allTodos
+			// jsonErr := json.NewDecoder(reponse.Body).Decode(&body)
+
+			// if jsonErr != nil {
+			// 	t.Fatal(jsonErr)
+			// }
 
 			if status := reponse.Code; status != tt.args.expCode {
 				t.Errorf("handler returned wrong status code: got %v want %v",
